b79c148c6aa49eb4b5cd659df064aa8cefa396dd
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-03-21 01:47:05 +0600

    Add commit

--- client/src/components/Banner.tsx
+++ client/src/components/Banner.tsx
@@ -1,40 +1,78 @@
+import { Banner2 } from "../assets";
 
-import {  Banner2 } from "../assets"
 function Banner() { 
-
-
-    return (
-    <div className="w-screen h-[85vh] overflow-hidden flex bg-cover  bg-center bg-no-repeat "
-    style={{ backgroundImage: `url(${Banner2})`}}
+  return (
+    <div
+      className="w-screen h-[85vh] overflow-hidden flex s bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${Banner2})` }}
     >
-    <div className="flex flex-1 inset-0 bg-black/40 gap-0 justify-end">
-
-            <div className="flex flex-col gap-0 w-1/2  items-end justify-center lg:mt-10 ml-4" >
-                <div className=" w-[500px]" > <h1 className="lg:text-[50px] font-bold text-white mb-[-1rem] ">Get  25% OFF</h1> </div>
-                <div className="flex items-start  w-[500px]"><h1 className="lg:text-[50px] font-bold text-start text-white mb-[-1rem] ">On Your </h1> </div>
-                <div className="flex items-center w-[500px]"><h1 className="lg:text-[50px] font-bold  text-white ">First Purchase</h1></div>
-                <div className="flex items-start  w-[500px] mt-2"><h1 className="text-[20px] font-normal text-white ">Use the code: <span className="text-1xl font-semibold">150FF</span></h1></div>
-                <div className="flex gap-2  w-[500px] items-start ">
-                    <p className="text-[20px] text-white ">Easy and FREE Returns</p>
-                </div>
-                <div className="flex gap-2  w-[500px] items-start lg:mt-[-5px] "><p className="text-[14px] text-white">*T&Cs apply</p></div>
-
-               <div className=" w-[500px] lg:mt-5"><button className=" w-1/2 cursor-pointer 
-               hover:bg-white hover:text-black hover:duration-300 py-2 px-4 flex justify-center
-                items-start border bg-transparent text-white
-                border-white rounded-[5px] text-xl font-normal text-start ">Shop Now</button></div> 
-            </div>
-            <div className="w-1/2 flex lg:mr-[20px] mt-8 items-start">
+      <div
+        className="flex flex-1  inset-0 bg-black/70 gap-0 
+                   justify-end  items-center "
+      >
+        <div
+          className="flex flex-col gap-2 sm:gap-2 md:gap-1 lg:gap-1 items-center sm:ml-0 md:ml-0 lg:ml-15  w-full max-w-[500px]">
+         <div className="flex  gap-2 lg:flex-col    ">
             
-            </div>
-        
-            </div>
 
+         <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+          <h1
+  className="text-[30px] sm:text-[50px] md:text-[40px] lg:text-[50px] font-bold text-white 
+             mb-[-1rem] text-start sm:text-center md:text-center"
+>
+  Get 25% OFF
+</h1>
+          </div>
+          <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+            <h1
+              className="text-[30px] sm:text-[50px] md:text-[40px] lg:text-[50px] font-bold text-start text-white 
+                         mb-[-1rem] sm:text-center md:text-center"
+            >
+              On Your
+            </h1>
+          </div>
+
+         </div>
+          
+          <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+            <h1
+              className="text-[30px] sm:text-[50px] md:text-[40px] lg:text-[50px] font-bold text-white 
+                         text-start sm:text-center md:text-center"
+            >
+              First Purchase
+            </h1>
+          </div>
+          <div className="flex flex-col gap-0 sm:gap-0  md:gap-0.5 lg:gap-1">
+          <div className="flex  items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+            <h1
+              className="text-[15px] sm:text-[18px] md:text-[20px] lg:text-[20px] font-normal text-white 
+                         text-start sm:text-center md:text-center"
+            >
+              Use the code: <span className="text-sm font-semibold">150FF</span>
+            </h1>
+          </div>
+          <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+            <p className="text-[14px] sm:text-[16px] md:text-[18px] lg:text-[18px] font-normal text-white text-start sm:text-center">Easy and FREE Returns</p>
+          </div>
+          <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">
+            <p className="text-[14px] text-white text-start sm:text-center">*T&Cs apply</p>
+          </div>
+          </div>
+          
+          <div className="flex items-center sm:items-center lg:w-[500px] lg:justify-start lg:ml-50">       <button
+              className=" w-[180px] sm:w-[250px] md:w-[250px] lg:[250px]  cursor-pointer 
+                         hover:bg-white hover:text-black hover:duration-300 py-2 px-4 
+                         flex justify-center items-center border bg-transparent text-white 
+                         border-white rounded-[5px] text-xl font-normal text-start mt-5 "
+            >
+              Shop Now
+            </button>
+          </div>
+        </div>
+        <div className="lg:w-1/2 md:w-0 sm:w-0 sm:h-0 flex lg:mr-[20px] sm:mx-auto mt-8 items-start"></div>
+      </div>
     </div>
-
-    
-
-    )
+  );
 }
 
-export default Banner
+export default Banner;
--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -1,10 +1,10 @@
+import { useState } from 'react'; // Add useState for managing mobile menu state
 import { useNavigate } from 'react-router-dom';
 import { websiteLogo } from '../assets/index';
 import { cart } from '../assets/index';
 import { shopingBag } from '../assets/index';
 import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-
 
 type Product = {
   _id: string,
@@ -15,6 +15,7 @@ type Product = {
   oldPrice: number,
   price: number,
 }
+
 type rootState = {
   Hatbazar: {
     productsData: Product[]
@@ -23,56 +24,124 @@ type rootState = {
 
 function Header() {
   const productData = useSelector((state: rootState) => state.Hatbazar.productsData);
-  console.log(productData);
-  const Navigate = useNavigate();
-  return (
-    <div className="w-[95vw] m-auto bg-white z-40  h-20 border-b-[1px]
-     border-b-gray-800 sticky top-0 ">
-      <div className="flex justify-between lg:h-20 items-center mx-auto  ">
-        <div className='ml-0'>
-          <img className="w-auto h-60" src={websiteLogo} alt="logo" />
-        </div>
-        <div className='mr-3 flex gap-5 lg:mr-[5rem]'>
-          <ul className="flex gap-8 items-center lg:mr-[5rem]" >
-            <li 
-            onClick={() => Navigate('/')}
-            className="
-            text-base text-black font-semibold hover:text-orange-900 *
-            hover:underline underline-offset-2 decoration-[1px]
-            cursor-pointer duration-300">Home</li>
-            <li className="
-            text-base text-black font-semibold hover:text-orange-900 *
-            hover:underline underline-offset-2 decoration-[1px]
-            cursor-pointer duration-300">Pages</li>
-            <li className="
-            text-base text-black font-semibold hover:text-orange-900 *
-            hover:underline underline-offset-2 decoration-[1px]
-            cursor-pointer duration-300">Shop</li>
-            <li className="
-            text-base text-black font-semibold hover:text-orange-900 *
-            hover:underline underline-offset-2 decoration-[1px]
-            cursor-pointer duration-300">Element</li>
-            <li className="
-            text-base text-black font-semibold hover:text-orange-900 *
-            hover:underline underline-offset-2 decoration-[1px]
-            cursor-pointer duration-300">Blog</li>
+  const navigate = useNavigate();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false); // State for mobile menu
 
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen); // Toggle mobile menu visibility
+  };
+
+  return (
+    <div className="w-full bg-white z-40 h-20 border-b-[1px] border-b-gray-800 sticky top-0">
+      <div className="container mx-auto flex justify-between items-center h-20 px-4">
+        {/* Logo */}
+        <div className="flex-shrink-0 w-[200px] h-[150px]">
+          <img className="w-full h-full" src={websiteLogo} alt="logo" />
+        </div>
+
+        {/* Navigation Links (Desktop) */}
+        <div className="hidden md:flex gap-8 items-center">
+          <ul className="flex gap-8">
+            <li
+              onClick={() => navigate('/')}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Home
+            </li>
+            <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">
+              Pages
+            </li>
+            <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">
+              Shop
+            </li>
+            <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">
+              Element
+            </li>
+            <li className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300">
+              Blog
+            </li>
           </ul>
-          <div className='relative '>
-            <img src={cart} className='w-8 cursor-pointer' />
-           
+        </div>
+
+        {/* Cart and Shopping Bag Icons */}
+        <div className="flex items-center gap-4">
+          <div className="relative">
+            <img src={cart} className="w-8 cursor-pointer" alt="cart" />
           </div>
           <Link to="/cart">
-              <div className='relative'>
-              <img className="w-8 cursor-pointer" src={shopingBag} alt="googleLogo" />
-              <span
-                className='absolute w-4 left-2 top-2.5 text-sm flex items-center
-                justify-center font-semibold rounded-4xl text-black'
-                >{productData.length}</span>
+            <div className="relative">
+              <img className="w-8 cursor-pointer" src={shopingBag} alt="shopping bag" />
+              <span className="absolute w-4 left-2 top-2.5 text-sm flex items-center justify-center font-semibold text-black">
+                {productData.length}
+              </span>
             </div>
           </Link>
-        </div>    
+        </div>
+
+        {/* Mobile Menu Button */}
+        <div className="md:hidden flex items-center">
+          <button
+            onClick={toggleMobileMenu} // Add onClick handler to toggle mobile menu
+            className="text-black focus:outline-none"
+          >
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
       </div>
+
+      {/* Mobile Menu (Dropdown) */}
+      {isMobileMenuOpen && (
+        <div className="md:hidden bg-white border-t border-gray-200">
+          <ul className="flex flex-col gap-4 p-4">
+            <li
+              onClick={() => {
+                navigate('/');
+                toggleMobileMenu(); // Close menu after navigation
+              }}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Home
+            </li>
+            <li
+              onClick={toggleMobileMenu}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Pages
+            </li>
+            <li
+              onClick={toggleMobileMenu}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Shop
+            </li>
+            <li
+              onClick={toggleMobileMenu}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Element
+            </li>
+            <li
+              onClick={toggleMobileMenu}
+              className="text-base text-black font-semibold hover:text-orange-900 hover:underline underline-offset-2 decoration-[1px] cursor-pointer duration-300"
+            >
+              Blog
+            </li>
+          </ul>
+        </div>
+      )}
     </div>
   );
 }
