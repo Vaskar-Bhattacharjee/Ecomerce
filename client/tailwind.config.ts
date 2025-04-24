e02ba8b487db81c2b6a41de58e879f3b3c87924e
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-04-24 10:41:43 +0600

    Add some another spice

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "githubColor": "#24292e",
+      }
+    },
   },
   plugins: [],
 }
