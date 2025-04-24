e3de6a3299b353b1e871c7fd1ec7afa5ad701874
Vaskar-Bhattacharjee <vaskarbhattacharjee03@gmail.com>
2025-04-24 09:51:14 +0600

    Add debuging tailwind config error

--- client/tailwind.config.ts
+++ client/tailwind.config.ts
@@ -1,16 +1,11 @@
-import type { Config } from 'tailwindcss';
-
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './index.html',
-    './src/**/*.{js,ts,jsx,tsx}',
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {
-      colors: {
-        'githubColor': '#0D1117',
-    },
+    extend: {},
   },
-},
   plugins: [],
-} satisfies Config;
+}
